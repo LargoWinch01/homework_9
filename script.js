9be428a76377a551292fdab01f27e6697016376a
LargoWinch01 <tokorobakidze7@gmail.com>
2024-09-10 16:12:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+// პირველი ვარიანტი
 num=prompt('ჩაწერეთ რაიმე რიცხვი:  ')
 
-if(num%2===0 && typeof(num)!='number'){
+if(num%2 === 0){
     console.log('რიცხვი ლუწია');
-}else if(num%2!=0 && typeof(num)!='number'){
+}else if(num%2 === 1){
     console.log('რიცხვი კენტია');
 }else{
     console.log('შეყვანილია არასწორი ფორმატის ინფორმაცია');
+}
+
+
+// მეორე ვარიანტი
+let num=prompt('ჩაწერეთ რაიმე რიცხვი:  ')
+
+switch (num % 2)
+{
+    case 1 :
+        console.log('რიცხვი კენტია');
+        break;
+    case 0  :
+        console.log('რიცხვი ლუწია');
+        break;
+    default:
+        console.log('შეყვანილია არასწორი ინფორმაცია');
+        break;
 }
